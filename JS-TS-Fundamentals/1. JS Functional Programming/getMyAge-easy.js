88238ae9382aadd178234b7db99b86792fbf5292
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-12 09:13:16 +0100

    updates

--- JS-TS-Fundamentals/1. JS Functional Programming/getMyAge-easy.js
+++ JS-TS-Fundamentals/1. JS Functional Programming/getMyAge-easy.js
@@ -1,4 +1,8 @@
 function getMyAge(input) {
+  const inputType = Object.prototype.toString.call(input);
+
+  console.log(inputType);
+
   // 1. typy
   // jeśli typ date => handleDate
   // jeśli typ string => handleString
@@ -11,18 +15,18 @@ function getMyAge(input) {
   // nie można wprowadzić roku przed 1990
 
   const currentYear = new Date().getFullYear();
-  const yourBirthYear = new Date(String(input)).getFullYear()
-  return currentYear - yourBirthYear		
+  const yourBirthYear = new Date(String(input)).getFullYear();
+  return currentYear - yourBirthYear;
 }
 
-const isObject = (obj) => {
-  return Object.prototype.toString.call(obj) === '[object Date]';
-};
-  
-  const result1 = getMyAge(new Date(1990, 1, 1));
-  const result2 = getMyAge("1990");
-  const result3 = getMyAge(1990);
-  
-  console.log(result1)
-  console.log(result2)
-  console.log(result3)
+// const isObject = (obj) => {
+//   return Object.prototype.toString.call(obj) === "[object Date]";
+// };
+
+const result1 = getMyAge(new Date(1990, 1, 1));
+const result2 = getMyAge("1990");
+const result3 = getMyAge(1990);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
