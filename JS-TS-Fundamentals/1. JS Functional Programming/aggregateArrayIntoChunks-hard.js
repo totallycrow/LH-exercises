48f0b84849daa5458bda044c9368d48f9348c1d5
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-18 14:07:15 +0100

    work

--- JS-TS-Fundamentals/1. JS Functional Programming/aggregateArrayIntoChunks-hard.js
+++ JS-TS-Fundamentals/1. JS Functional Programming/aggregateArrayIntoChunks-hard.js
@@ -1,83 +1,75 @@
 const alphabet = "abcdefghijklmnoprstuwxyz".split("");
+// const alphabet = "abcd".split("");
 
 const aggregateIntoChunks = (array) => {
-  console.log("start");
-  console.log(array);
-  // return ...
   const MINLENGTH = 4;
   const MAXLENGTH = 7;
 
-  const clonedArray = [...array];
-  let arrayLength = clonedArray.length;
-
-  
-  let result = [];
-  
-
-  // while (arrayLength >= MINLENGTH && arrayLength ) {
-  //   const randomChunk = getRandom(MINLENGTH, MAXLENGTH+1);
-  //   console.log(randomChunk);
-  //   let chunkArray = []
-
-  //   for (let i = 0; i < randomChunk; i++){
-      
-  //     chunkArray.push(clonedArray[i])
-  //     clonedArray.splice(i, 1);
-
-  //   }
-  //   arrayLength = arrayLength - randomChunk
-  //   result.push(chunkArray)
-  //   chunkArray = []
-  // }
-
-  // 
-  // Check if arraylength > 7
-  // generate random number
-  // deduct from arraylength
-  // check if arraylength > 4
-  // if not, re-genarate
-
-
-
-
-
-
-
-
-  
-  return result
-  console.log(arrayLength);
+  return splitArray(array, MINLENGTH, MAXLENGTH);
 };
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function splitArray(array) {
+function splitArray(array, min, max) {
+  console.log("FUNCTION START");
+  let arrayLength = [...array].length;
+  const clonedArray = [...array];
+  let result = [];
 
-  inputArrayLength = array.length
-  isNextValid = true;
-
-  while(isNextValid) {
-    const randomChunk = getRandom(MINLENGTH, MAXLENGTH+1);
-    lengthCheck = inputArrayLength - randomChunk
-
-    
+  if (array.length <= min) {
+    return array;
   }
 
- 
+  let i = 0;
+  while (i < array.length) {
+    console.log("LOOP START");
+    console.log("INDEX:");
+    console.log(i);
 
+    console.log(i < array.length);
+    let randomChunk = getRandom(min, max + 1);
+    let difference = arrayLength - randomChunk;
+    console.log("RANDOM");
+    console.log(randomChunk);
+    console.log("ARRAY LENGTH");
+    console.log(arrayLength);
+    console.log("DIFFERENCE");
+    console.log(difference);
 
+    if (difference >= min || difference === 0) {
+      console.log("CLONE ARRAY BEFORE SLICE");
+      console.log(clonedArray);
+      let push = clonedArray.slice(i, i + randomChunk);
+      console.log("PUSH");
+      console.log(push);
+      console.log("CLONE ARRAY AFTER SLICE");
+      console.log(clonedArray);
 
-
-
+      result.push(push);
+      arrayLength -= randomChunk;
+      i += randomChunk;
+    }
+  }
+  return result;
 }
 
-
 const chunks = aggregateIntoChunks(alphabet);
+const chunksNums = aggregateIntoChunks([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 12, 1231, 13, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1,
+]);
 
 // chunks:
 // [[a,b,c,d,e,f],[g,h,i,j,k],[l,m,n,o,p,r,s],[t,u,w,x,y,z]]
 
 // ********** TEST ***************
+console.log("FINAL CHUNKS VAR");
 console.log(chunks);
+console.log(chunksNums);
+
+// FINAL CHECKS
+
+console.log(chunks.every((el) => el.length >= 4 || el.length <= 7));
+console.log(chunksNums.every((el) => el.length >= 4 || el.length <= 7));
