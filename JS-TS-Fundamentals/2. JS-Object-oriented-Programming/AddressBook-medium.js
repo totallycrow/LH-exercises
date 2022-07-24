7eaeb09b6b2262f95d22a554736d24a4d4b54264
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-24 11:56:33 +0100

    work

---
+++ JS-TS-Fundamentals/2. JS-Object-oriented-Programming/AddressBook-medium.js
@@ -0,0 +1,19 @@
+// Stwórz strukturę danych związaną z biblioteką, pełen opis znajduję się w kodzie poniżej
+//  Wypracuj obiekt charakteryzujący pojedyńczy kontakt
+//  Wypracuj obiekt charakteryzujący grupę kontakt
+//  Wypracuj obiekt charakteryzujący książkę adresową
+
+class Contact {
+    // Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji i utworzenia, uuid
+    // Ma umożliwiać: aktualizację datę modyfikacji, pozwalac na modyfikację imienia, nazwiska oraz adresu email
+}
+
+class Group {
+    // Ma mieć: listę kontaktów oraz nazwę grupy oraz uuid
+    // Ma umożliwiać: zmianę nazwy grupy, można dodać lub usunac kontakt z grupy, można sprawdzić czy kontakt istnieje w grupie
+}
+
+class AddressBook {
+// Ma mieć: listę wszystkich kontaktów, listę grup kontaktów 
+// Ma umożliwiać: szukanie kontaktu po frazie, dodawanie/usuwanie/modyfikacje nowych kontaktów, dodawanie/usuwanie/modyfikacje nowych grup
+}
