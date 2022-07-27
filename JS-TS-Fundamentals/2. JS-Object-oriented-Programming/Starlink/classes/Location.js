ed6ff02185837602c2b7f5e3b2d084952114ce60
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-27 10:27:06 +0100

    work

---
+++ JS-TS-Fundamentals/2. JS-Object-oriented-Programming/Starlink/classes/Location.js
@@ -0,0 +1,31 @@
+import Validator from "../../Validator.js";
+
+class Location {
+  constructor(height, coordinates = { x: "", y: "" }) {
+    Validator.validateInt(height);
+    Validator.validateCooridnatesObject(coordinates);
+
+    this.height = height;
+    this.coordinates = coordinates;
+  }
+
+  setHeight = (height) => {
+    Validator.validateInt(height);
+    this.height = height;
+  };
+  setCoordinates = (coordinates) => {
+    Validator.validateCooridnatesObject(coordinates);
+    this.coordinates = coordinates;
+  };
+}
+
+// TEST
+
+const testLoc = new Location(1200, { x: "150.2", y: "121.35" });
+
+console.log(testLoc);
+testLoc.setHeight(5);
+console.log(testLoc);
+
+testLoc.setCoordinates({ x: "650.2", y: "621.35" });
+console.log(testLoc);
