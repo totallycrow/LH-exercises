24dee3e622d993890f86fcb8081b75e500790b48
Adrian Gajda <adrian.gajda@hotmail.com>
2022-08-21 22:21:08 +0100

    work

---
+++ JS-TS-Fundamentals/4. Async/CV-CLI/bin/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+"use strict";
+const yargs = require("yargs");
+const options = yargs
+    .usage("Usage: -n <name>")
+    .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+    .argv;
+const greeting = `Hello, ${options.name}!`;
+console.log(greeting);
